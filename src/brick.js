4d9f3b06bb1f6174460c81ba247e3f40da15b3ed
ablades <audaris@gmail.com>
2020-04-21 09:49:42 -0700

    create brick class

---
+++ src/brick.js
@@ -0,0 +1,19 @@
+class Brick {
+  // Init brick object
+  constructor(x, y, width = 75, height = 20, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  // Render brick
+  render(ctx) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
