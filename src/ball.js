4778515228d303a881be055f9a71dd10bc37c9b3
ablades <audaris@gmail.com>
2020-04-21 09:32:03 -0700

    git push origin master

--- src/ball.js
+++ src/ball.js
@@ -1,4 +1,5 @@
 class Ball {
+  // Creates instance of ball
   constructor(x, y, dx = 2, dy = -1, radius = 10, stroke = '#d400ff', color = '#ffffff') {
     this.x = x;
     this.y = y;
@@ -9,15 +10,21 @@ class Ball {
     this.color = color;
   }
 
+  // Change positon of ball's x and y
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+  }
 
+  // Renders ball with a given context
   render(ctx) {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-      ctx.fillStyle = this.color;
-      ctx.strokeStyle = this.stroke;
-      ctx.lineWidth = 2;
-      ctx.fill();
-      ctx.stroke();
-      ctx.closePath();
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.strokeStyle = this.stroke;
+    ctx.lineWidth = 2;
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
   }
 }
